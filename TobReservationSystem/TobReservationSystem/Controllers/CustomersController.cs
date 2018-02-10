22efe4914bf2ee9fd7f2fec86e656ff8e64ebb35
Alex Lawrence <alexlawrence01@live.co.uk>
2018-02-10 23:05:49 +0000

    Simple demonstration of MVC in action

---
+++ TobReservationSystem/TobReservationSystem/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TobReservationSystem.Models;
+
+namespace TobReservationSystem.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        // GET: Customers/Details/id
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+                return View(customer);
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>
+
+            {
+                new Customer { Id = 1, Name = "James Smith", Birthdate = new DateTime(1989, 3, 18) },
+                new Customer { Id = 2, Name = "Harry Anderson", Birthdate = new DateTime(1991, 11, 29) }
+            };
+
+            return customers;
+        }
+    }
+}
